11b16306336f5951fbe83591ea2408d3af4628de
Kai Pham <duykhangpham201@gmail.com>
2022-01-13 21:40:03 -0500

    setting up

---
+++ src/components/ConnectButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Button } from "react-bootstrap";
+
+const ConnectButton = () => {
+    return (
+        <Button variant='success'>
+            <strong>Connect To Wallet</strong>
+        </Button>
+    )
+}
+
+export default ConnectButton
