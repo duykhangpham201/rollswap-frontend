bcdc7658413657c5f46b9430d44ba08b9a6018fd
Kai Pham <duykhangpham201@gmail.com>
2022-01-16 09:23:02 -0500

    finalize

--- src/components/SwapForm.js
+++ src/components/SwapForm.js
@@ -1,30 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Form, Container, Card, Button, Row, Col } from "react-bootstrap";
 import { ethers } from "ethers";
-import { PAIR_CONTRACT } from "../utils/constants";
 
 const SwapForm = ({ address, RollswapPair, Ohm, Time }) => {
   const [token0, setToken0] = useState(null);
-  const contractAddress = PAIR_CONTRACT;
-  const [allowance, setAllowance] = useState("unapproved");
-  const maxUint256 = 100;
-
-  const approveHandler = async (e) => {
-    try {
-      e.preventDefault();
-      await Ohm.approve(
-        contractAddress,
-        ethers.utils.parseEther(maxUint256.toString())
-      );
-
-      await Time.approve(
-        contractAddress,
-        ethers.utils.parseEther(maxUint256.toString())
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   const swap = async (e) => {
     try {
@@ -38,22 +17,6 @@ const SwapForm = ({ address, RollswapPair, Ohm, Time }) => {
     }
   };
 
-  const checkAllowance = async () => {
-    try {
-      const OhmAllow = await Ohm.allowance(contractAddress, address);
-      const TimeAllow = await Time.allowance(contractAddress, address);
-      if (OhmAllow !== 0 && TimeAllow !== 0) {
-        setAllowance("approved");
-      }
-      console.log(allowance);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    checkAllowance();
-  });
   return (
     <Card>
       <Form onSubmit={swap}>
@@ -72,10 +35,6 @@ const SwapForm = ({ address, RollswapPair, Ohm, Time }) => {
                 <Button variant="success" type="submit" class>
                   Swap
                 </Button>
-              </Col>
-              <Col xs={4}></Col>
-              <Col xs={3}>
-                <Button onClick={approveHandler}>Approve</Button>
               </Col>
             </Row>
           </div>
